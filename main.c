2d66e39a750040070ac7fc03464e8f1c0a3a695d
Robson <robsoncouto@outlook.com>
2017-01-01 22:40:30 -0300

    Display works.

--- main.c
+++ main.c
@@ -1,12 +1,18 @@
+#define F_CPU 8000000UL
+
 #include <avr/io.h>
 #include "i2clib/i2c_master.h"
 #include "display.h"
+#include "ds1302.h"
+#include <util/delay.h>
 
 
+uint8_t test_dig=0;
 int main (void){
-  i2c_init();
+  //i2c_init();
   displayinit();
-  while(1){
+  displayWrite(12, 00);
+    while(1){
 
   }
 }
