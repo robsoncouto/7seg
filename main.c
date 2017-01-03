9bbe58a70db9b0cf9cec040bb472abcfd0f3cde4
Robson <robsoncouto@outlook.com>
2017-01-03 17:35:20 -0300

    Library tested to work after some fixes

--- main.c
+++ main.c
@@ -1,18 +1,39 @@
 #define F_CPU 8000000UL
 
 #include <avr/io.h>
-#include "i2clib/i2c_master.h"
 #include "display.h"
-#include "ds1302.h"
+#include "RTC/rtc.h"
 #include <util/delay.h>
 
 
 uint8_t test_dig=0;
 int main (void){
-  //i2c_init();
   displayinit();
   displayWrite(12, 00);
+  struct rtc_time ds1302;
+	struct rtc_time *rtc;
+	rtc = &ds1302;
+
+	ds1302_init();
+	ds1302_update(rtc);   // update all fields in the struct
+
+  //ds1302_write_byte(w_protect,0x00);
+  //ds1302_set_time(rtc, SEC, 01);	//set the seconds to 31
+  //ds1302_set_time(rtc, SEC, 01);	//set the seconds to 31
+  //ds1302_set_time(rtc, MIN, 01);	//set the seconds to 31
+  //ds1302_set_time(rtc, HOUR, 01);	//set the seconds to 31
+
+  //test_dig=ds1302_read_byte(min_r|0x01);
+  //displayWrite(0,test_dig);
+  ds1302_update(rtc);
+
     while(1){
+      //dsReset();
+      ds1302_update_time(rtc, SEC);	//read the seconds
+      ds1302_update_time(rtc, MIN);	//read the seconds
+      ds1302_update_time(rtc, HOUR);	//read the seconds
+      displayWrite(rtc->minute,rtc->second);
+      _delay_ms(500);
 
   }
 }
