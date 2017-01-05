bb3693661cda5ad3a2dcb3b7a39ef800af6153f8
Robson <robsoncouto@outlook.com>
2017-01-04 23:10:42 -0300

    Added ISR for buttons

--- main.c
+++ main.c
@@ -4,15 +4,37 @@
 #include "display.h"
 #include "RTC/rtc.h"
 #include <util/delay.h>
+#include <avr/interrupt.h>
 
+struct rtc_time ds1302;
+struct rtc_time *rtc;
 
-uint8_t test_dig=0;
+ISR(INT0_vect){
+  uint8_t press=0;
+    while(!(PIND&(1<<PIND2))){
+      press++;
+    }
+    if(press>500){
+      ds1302_update_time(rtc, MIN);	//read the seconds
+      ds1302_set_time(rtc, MIN, rtc->minute+1);	//set the seconds to 31
+    }
+}
+ISR(INT1_vect){
+  uint8_t press=0;
+    while(!(PIND&(1<<PIND2))){
+      press++;
+    }
+    if(press>500){
+      ds1302_update_time(rtc, HOUR);
+      ds1302_set_time(rtc, HOUR, rtc->hour+1);
+    }
+}
+
 int main (void){
+  rtc = &ds1302;
   displayinit();
   displayWrite(12, 00);
-  struct rtc_time ds1302;
-	struct rtc_time *rtc;
-	rtc = &ds1302;
+
 
 	ds1302_init();
 	ds1302_update(rtc);   // update all fields in the struct
@@ -25,7 +47,7 @@ int main (void){
 
   //test_dig=ds1302_read_byte(min_r|0x01);
   //displayWrite(0,test_dig);
-  ds1302_update(rtc);
+  //ds1302_update(rtc);
 
     while(1){
       //dsReset();
